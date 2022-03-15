c8869a10861e0af29592eba52bde7e616420abc5
RenieG <igakaho@yahoo.com>
2022-03-15 13:03:09 -0500

    Initial commit

---
+++ src/CountProgram.java
@@ -0,0 +1,16 @@
+public class CountProgram {
+    public static void main(String [] args) {
+
+        //for loop
+        //Needs to print numbers from 0-10 (Inclusive)
+        //Start at 0
+        //Terminate after 10
+        //Increment a value
+
+        for (int i=0; i<11; i++)
+
+        System.out.println("Numbers:" + i);
+        System.out.println("\n");
+    }
+
+}
